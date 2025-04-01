c19dde07f3f9260e73157a8c31f8b5c5aa952398
Peter Phanouvong <peterphanouvong@gmail.com>
2025-04-02 10:24:28 +1100

    init

---
+++ kindeSrc/environment/workflows/PasswordWorkflow.ts
@@ -0,0 +1,33 @@
+import {
+  onNewPasswordProvidedEvent,
+  WorkflowSettings,
+  WorkflowTrigger,
+} from "@kinde/infrastructure";
+
+export const workflowSettings: WorkflowSettings = {
+  id: "newPassword",
+  name: "New password",
+  failurePolicy: {
+    action: "stop",
+  },
+  trigger: WorkflowTrigger.NewPasswordProvided,
+  bindings: {
+    "kinde.secureFetch": {}, // Required for external API calls
+    "kinde.widget": {}, // Required to invalidate the form
+    "kinde.localization": {}, // Required for localization
+    "kinde.mfa": {}, // Required for multi-factor authentication
+  },
+};
+
+export default async function PasswordWorkflow({
+  context,
+}: onNewPasswordProvidedEvent) {
+  const isMinCharacters = context.auth.firstPassword.length >= 50;
+
+  if (isMinCharacters) {
+    kinde.widget.invalidateFormField(
+      "p_password_1",
+      "Provide a password at least 50 characters long"
+    );
+  }
+}
