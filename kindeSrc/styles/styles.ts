804ed8b747e4e1e6ac3a8002f831e031a328e65a
Peter Phanouvong <peterphanouvong@gmail.com>
2025-04-09 11:16:54 +1000

    styles

--- kindeSrc/styles/styles.ts
+++ kindeSrc/styles/styles.ts
@@ -2,43 +2,24 @@
 const kindeVariables = {
   baseFontFamily:
     "-apple-system, system-ui, BlinkMacSystemFont, Helvetica, Arial, Segoe UI, Roboto, sans-serif",
-  controlSelectTextBorderRadius: "99px",
-  buttonPrimaryBackgroundColor:
-    "linear-gradient(153deg, #F0B376 23.13%, #F29665 69.42%, #DF4D51 177.44%)",
-  buttonPrimaryColor: "#fff",
-  buttonBorderRadius: "99px",
-  buttonSecondaryBackgroundColor: "#fff",
-  buttonSecondaryBorderWidth: "1px",
-  buttonSecondaryBorderColor: "#e9edec",
-  buttonSecondaryBorderStyle: "solid",
-  buttonSecondaryBorderRadius: "99px",
+  controlSelectTextBorderRadius: "16px",
+  buttonPrimaryColor: "#0F0F0F",
+  buttonBorderRadius: "16px",
+  baseColor: "#F5F5F5",
 } as const;
 
 export const getStyles = (): string => `
   :root {
+    --kinde-base-color: ${kindeVariables.baseColor};
     --kinde-base-font-family: ${kindeVariables.baseFontFamily};
     --kinde-control-select-text-border-radius: ${kindeVariables.controlSelectTextBorderRadius};
-    --kinde-button-primary-background-color: ${kindeVariables.buttonPrimaryBackgroundColor};
     --kinde-button-primary-color: ${kindeVariables.buttonPrimaryColor};
     --kinde-button-border-radius: ${kindeVariables.buttonBorderRadius};
-    --kinde-button-secondary-background-color: ${kindeVariables.buttonSecondaryBackgroundColor};
-    --kinde-button-secondary-border-width: ${kindeVariables.buttonSecondaryBorderWidth};
-    --kinde-button-secondary-border-color: ${kindeVariables.buttonSecondaryBorderColor};
-    --kinde-button-secondary-border-style: ${kindeVariables.buttonSecondaryBorderStyle};
-    --kinde-button-secondary-border-radius: ${kindeVariables.buttonSecondaryBorderRadius};
+
   }
 
-  [data-kinde-choice-separator] {
-    text-transform: uppercase;
+  [data-kinde-button-variant=primary] { 
+    background: linear-gradient(153deg, #F0B376 23.13%, #F29665 69.42%, #DF4D51 177.44%);
   }
 
-  [data-kinde-layout-auth-buttons] { 
-    display: flex;
-    justify-content: center;
-  }
-
-  [data-kinde-layout-auth-buttons-item] {
-    width: 3rem;
-    height: 3rem;
-  }
 `;
